0f60ff76707a104bff372f172aa3c73e7a3f6278
Aaron Thibeaux <aktbomusic@gmail.com>
2022-02-01 13:38:40 -0600

    Organizing Hero Elements

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -3,19 +3,23 @@ import "./Hero.css";
 import { FaEnvelope } from "react-icons/fa";
 import { IconContext } from "react-icons/lib";
 import "./Moon.png";
+import "./Sky.png";
 
 const Hero = () => {
   return (
     <div className="hero-box">
-      <img src="Moon.png" alt="Moon" />
-
-      <p>About Me</p>
-      <header>Aaron Thibeaux</header>
-      <p>
-        Asdfa ajfajf asjfjsfn sjdfnfnjaf fjasdfjfjsdf fjsdfdjsfjdsnfjsdf
-        fjdsnfjnsdf fsdjfnjdfnf fjsjfnsnfjnsdf dsfnsdjfsdjfsdf sdfjnsdjfnsdnfs
-        dfjsdnfndsfnsd sdjfsdnjfnajnfjn.
-      </p>
+      {/* <img src={require("./Sky.png")} alt="Sky" className="sky-pic" /> */}
+      <p className="large-heading"> Developer</p>
+      {/* <img src={require("./Moon.png")} alt="Moon" className="moon-pic" /> */}
+      <div className="info-box">
+        <p className="p-heading">About Me</p>
+        <p className="header">Aaron Thibeaux</p>
+        <p className="text-block">
+          Specifies the alignment between the lines inside a flexible container
+          when the items do not use all available space. Specifies the alignment
+          for selected items inside a flexible container.
+        </p>
+      </div>
 
       <button className="contact-btn">
         <IconContext.Provider
