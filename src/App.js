d69a1e63722b13554df2a23a312726a58beb2f42
Aaron Thibeaux <aktbomusic@gmail.com>
2022-02-07 20:38:29 -0600

    Deleting Pullout File

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import Home from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import About from "./Pages/About";
-import Pullout from "./components/pullout/Pullout";
 
 function App() {
   return (
     <div>
-      {/* <Pullout /> */}
       <Navbar />
       <Hero />
       <Home />
